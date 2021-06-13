9af0b280b7be5a9a3d2eba971ea7cac2a8af1442
Julio Barrera <jcbarrera@protonmail.com>
2021-06-13 15:54:10 -0700

    Change constant to a most suitable name

--- swift/2d_array-ds.swift
+++ swift/2d_array-ds.swift
@@ -11,12 +11,12 @@ func hourglassSum(arr: [[Int]]) -> Int {
     var maximum = Int.min
     
     for i in 0..<arr.count - 2 {
-        for j in 0..<arr.count - 2{
-            let currentMax = arr[i][j] + arr[i][j+1] + arr[i][j+2] + 
+        for j in 0..<arr.count - 2 {
+            let currentSum = arr[i][j] + arr[i][j+1] + arr[i][j+2] + 
             arr[i+1][j+1] + 
             arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2]
             
-            maximum = max(currentMax, maximum)
+            maximum = max(currentSum, maximum)
         }
     }
     
